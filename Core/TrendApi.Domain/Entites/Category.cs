9ca37f130ea4af615459028cd15c9fc6797090cc
Mustafa Karadeniz <mustafa2005karadeniz@outlook.com>
2024-08-19 16:29:57 +0300

    Configuration

--- Core/TrendApi.Domain/Entites/Category.cs
+++ Core/TrendApi.Domain/Entites/Category.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TrendApi.Domain.Common;
+﻿using TrendApi.Domain.Common;
 
 namespace TrendApi.Domain.Entites
 {
---
+++ Infrastructure/TrendApi.Persistence/Configurations/BrandConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Bogus;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TrendApi.Domain.Entites;
+
+
+namespace TrendApi.Persistence.Configurations;
+
+public class BrandConfiguration : IEntityTypeConfiguration<Brands>
+{
+    public void Configure(EntityTypeBuilder<Brands> builder)
+    {
+        builder.Property(x => x.Name).HasMaxLength(256);
+        
+
+        Faker faker = new("tr");
+        Brands brand1 = new()
+        {
+            Id = 1,
+            Name = faker.Commerce.Department(),
+            CreatedDate = DateTime.Now,
+            IsDeleted = false
+        };
+        Brands brand2 = new()
+        {
+            Id = 1,
+            Name = faker.Commerce.Department(),
+            CreatedDate = DateTime.Now,
+            IsDeleted = false
+        };
+        Brands brand3 = new()
+        {
+            Id = 1,
+            Name = faker.Commerce.Department(),
+            CreatedDate = DateTime.Now,
+            IsDeleted = true
+        };
+        builder.HasData(brand1,brand2,brand3);
+    }
+}
