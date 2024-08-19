c2d372b91a689764ae4aa8d8ce666aa512ae25c8
Mustafa Karadeniz <mustafa2005karadeniz@outlook.com>
2024-08-19 14:46:36 +0300

    entities created

---
+++ Core/TrendApi.Domain/Entites/Brands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrendApi.Domain.Common;
+
+namespace TrendApi.Domain.Entites
+{
+    public class Brands : EntityBase
+    {
+        public Brands()
+        {
+            
+        }
+        public Brands(string name)
+        {
+            Name = name;
+        }
+        public required string Name { get; set; }
+    }
+}
