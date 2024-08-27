dad784aeed2e66851454224c0eb37a106af4aebe
Mustafa Karadeniz <mustafa2005karadeniz@outlook.com>
2024-08-27 12:49:55 +0300

    Token Service

---
+++ Core/TrendApi.Application/Interface/Tokens/ITokenService.cs
@@ -0,0 +1,14 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using TrendApi.Domain.Entites;
+
+namespace TrendApi.Application.Interface.Tokens;
+
+public interface ITokenService
+{
+    Task<JwtSecurityToken> CreateToken(User user, IList<string> roles);
+
+    string GenerateRefreshToken();
+
+    ClaimsPrincipal? GetPrincipalFromExpiredToken(string? token);
+}
